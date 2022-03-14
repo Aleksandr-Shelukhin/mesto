80974508e979783790a4fdfe0a82d33d50f166b0
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-03-14 20:36:33 +0300

    hotfix: исправить замечания после ревью

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+// popup
+//====================================
+
+let popup = document.querySelector('.popup');
+let editProfile = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let profileNameValue = profileName.textContent;
+let profileJobValue = profileJob.textContent;
+
+console.log(profileNameValue)
+
+function popupOpened() { // открытие попап
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileNameValue;
+  jobInput.value = profileJobValue;
+}
+
+function popupClose() { // закрытие попап
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    // передаем значение полей value в соответсвущие элементы
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    //=============================
+
+    popupClose() // закрытие попап
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+editProfile.addEventListener('click', popupOpened); // открытие попап при нажадтии на кнопку редактирования
+popupCloseButton.addEventListener('click', popupClose); // закрытие попап при нажадтии на крестик в диалоговом акне
+
+
+
+
+
+
+
+
+
