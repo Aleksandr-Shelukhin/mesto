b8e4c3c2b3c344bf241007a4522e888dde789a76
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-05-24 17:17:12 +0300

    develop: настроить webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,72 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин обработки HTML
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин очистки папки dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключите плагин обработки CSS
 
 module.exports = {
-  entry: {
-    main: './src/pages/index.js'
-  },
+  entry: { main: './src/pages/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'), // переписали точку выхода, используя утилиту path
     filename: 'main.js',
     publicPath: '',
+    clean: true
   },
-  mode: 'development',
+  mode: 'development', // добавили режим разработчика
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    open: true,
-    compress: true,
-    port: 8080
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    open: true, // сайт будет открываться сам при запуске npm run dev
+    hot: true // горячая перезагрузка браузера
   },
   module: {
-    rules: [{
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
+      // добавили правило для обработки файлов
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif)$/,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]'
+        }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash][ext]'
+        }
       },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          // добавьте объект options
+          options: { importLoaders: 1 }
+        },
+        // Добавьте postcss-loader
+        'postcss-loader']
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+    template: './src/index.html' // путь к файлу index.html
     }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
 
+    new CleanWebpackPlugin(), // использовали плагин очистки папки dist
+    new MiniCssExtractPlugin() // использовали плагина для объединения CSS файлов
   ]
 }
