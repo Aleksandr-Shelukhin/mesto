cfd10fbbb415060d3c5adb17df5fcd273583f46f
Aleksandr Shelukhin <mad.di.s@yandex.ru>
2022-06-02 21:58:59 +0300

    feature: сделать верстку под новый функционал

--- src/components/initial.js
+++ src/components/initial.js
@@ -10,28 +10,28 @@ export const validateSettings = {
 
 export const renderCards = [
   {
-    name: 'Эльбрус',
-    link: 'https://cdn.pixabay.com/photo/2021/01/29/12/27/mountain-5960840_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/arch'
   },
   {
-    name: 'Екатеринбург',
-    link: 'https://cdn.pixabay.com/photo/2022/01/07/02/48/ekaterinburg-6920943_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/animals'
   },
   {
-    name: 'Коломна',
-    link: 'https://cdn.pixabay.com/photo/2018/09/18/17/12/kolomna-3686685_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/people'
   },
   {
-    name: 'Москва',
-    link: 'https://cdn.pixabay.com/photo/2018/12/26/05/13/moscow-3895333_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/nature'
   },
   {
-    name: 'Санкт-Петербург',
-    link: 'https://cdn.pixabay.com/photo/2021/12/29/19/02/peter-6902548_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/sepia'
   },
   {
-    name: 'Владивосток',
-    link: 'https://cdn.pixabay.com/photo/2014/08/29/03/34/bridge-430446_960_720.jpg'
+    name: 'Рендом',
+    link: 'http://placeimg.com/720/720/tech'
   }
 ];
 
